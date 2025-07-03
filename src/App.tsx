d7926167703805bd0cdcdb817f165434c0812add
unknown <yasvanthhanumantu1@gmail.com>
2025-07-03 21:56:00 +0530

    redirection

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,21 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ProfileForm, ProfilePage, PageNotFound } from "./pages";
 import Navbar from "./components/Navbar";
+import { useSelector } from "react-redux";
+import type { RootState } from "./store";
 
 function App() {
+  const profile = useSelector((state: RootState) => state.profile.data);
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/profile-form" />} />
+        {profile ? (
+          <Route path="/" element={<Navigate to="/profile-page" />} />
+        ) : (
+          <Route path="/" element={<Navigate to="/profile-form" />} />
+        )}
         <Route path="/profile-form" element={<ProfileForm />} />
         <Route path="/profile-page" element={<ProfilePage />} />
         <Route path="*" element={<PageNotFound />} />
